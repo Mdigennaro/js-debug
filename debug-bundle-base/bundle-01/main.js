ee4e4a82889c6080ebda792ce193495310577097
Mdigennaro <michele.digennaro719@gmail.com>
2021-12-09 17:40:14 +0100

    fine milestone 1

--- debug-bundle-base/bundle-01/main.js
+++ debug-bundle-base/bundle-01/main.js
@@ -12,45 +12,65 @@
 /*
 // ESERCIZIO 1 
 
-for (let i = 0; i > 5; i++) {
+for (let i = 0; i < 5; i++) {
     console.log(i);
 }
 
 /* 
-Il ciclo non parte per I è minore di 6*/
+Questo codice serve per fare un ciclo da 0 a 5 
+Errore:
+1.Bisognava invertite il > in < a riga 15
+*/
 /*
 // ESERCIZIO 2
+const num = 4;
+
 function addIfEven(num) {
-    if (num % 2 = 0) {
+    if (num % 2 == 0) {
         return num + 5;
     }
     return num;
 }
+console.log(addIfEven(num));
+
 /*
 Questo codice serve a sommare 5 a tutti i numeri divisibili a 2
 Errori:
 1. Manca la costante Num 
 2. Manca un uguale nella condizione a riga 24
 */
+/*
 // ESERCIZIO 3
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 0; i <= 5; i++) {
         console.log(i);
     }
 }
+
+console.log(loopToFive());
+
+/*
+Questo codice serve per creare un loop fino a cinque 
+Errore:
+1. Bisogna invertire le virgole a riga 45 in ;
+2. Aggiungere l ' uguale dopo il minore a riga 45
+*/
+
 /*
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
 function displayEvenNumbers() {
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
+    for (let i = 1; i < numbers.length 
+        + 1; i++) {
+        if (i % 2 == 0) {
             evenNumbers.push(i);
         }
-        return evenNumbers;
     }
+    return evenNumbers;
 }
 displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+console.log(displayEvenNumbers());
 /*
 Questo codice serve a mettere in numeri pari di un array dentro un altro array.
 
@@ -60,7 +80,7 @@ Errori:
 3. Cancellare il ; dopo l'incremento a riga 42
 4. Cambiare il nome dell 'array numbers con la 'i' a riga 
 5. Aggiungere un altro = nella condizione di if a riga 43
-6. Eliminare il ; a riga 43
-7. Spostare il return a riga 46 fuori dal ciclo for
-
+6. Eliminare il ; nella condizione
+7. Spostare il return fuori dal ciclo for
+8. Nel ciclo for la variabile i deve essere ugale a 1 per non far restituire 0
 */
--- debug-bundle-base/bundle-02/main.js
+++ debug-bundle-base/bundle-02/main.js
@@ -9,49 +9,66 @@
 ​
 *******************************************************************************/
 
-
+/*
 // ESERCIZIO 1
 function checkAge() {
     const myAge = 32;
-    const message = '';
+    let message = '';
 
     if (myAge < 18) {
-        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+       return message = `Sei troppo giovane! Hai ${myAge} anni!`;
     } else {
-        message = 'Hai più di 18 anni!';
+        return message = 'Hai più di 18 anni!';
     }
 }
 checkAge();
 
+/*
+Questo codice serve per verificare se l' età e maggiore o minore di 18 anni
+Errori:
+1. Bisogna cambiare la costante message in variabile let a riga 16
+2. Manca il return a message a riga 19 e 21
+*/
+/*
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
 }
 printColorsNumber();
 
-
+/*
+Questo codice serve per contare il numero di colori presente nell 'array
+Errore:
+1. A riga 36 è scritta male la parola length
+*/
+/*
 // ESERCIZIO 3
 function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
+    const userNumber = parseInt(prompt('Inserisci un numero')) ;
     const total = userNumber + 12;
-
+    
     console.log(`Il risultato finale è ${total}`);
 }
 addNumbers();
 
-
+/*
+Questa funzione serve per sommare 12 al numero inserito dall'utente
+Errore:
+1. Nella costante userNumber a riga 48 il prompt deve essere inserito nella funzione parseInt
+*/
+/*
 // ESERCIZIO 4
 function checkAccess() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
     const userEmail = prompt('Inserisci il tuo indirizzo email');
-
-    let grantAccess = 'false';
-
+    
+    let grantAccess = false;
+    
     if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
+        grantAccess = true;
     }
-
+    
     if (grantAccess === true) {
         console.log('Accesso consentito!');
     } else {
@@ -60,34 +77,47 @@ function checkAccess() {
 }
 checkAccess();
 
+/*
+Questo codice serve per verificare la mail inserita dall'utente puo accedere
+Errore:
+1. La variabile booleana viene inserita come stringa e non come variabile sia a riga 66 e sia a riga 69
+*/
 
+/*
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
 function checkAccessImproved() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
-
+    
     const userEmail = prompt('Inserisci il tuo indirizzo email');
-
+    
     let grantAccess = false;
-
+    
     for (let i = 0; i < addresses.length; i++) {
-     const email = addresses[i];
+        const email = addresses[i];
         
-      if (userEmail.length > 5) {
+        if (userEmail.length > 5) {
             
-        if (email === userEmail) {
-        grantAccess = 'true';            
+            if (email === userEmail) {
+                grantAccess = 'true';            
+                
+            }
             
-      }
+        }
         
-    }
-
-    if (grantAccess) {
-        console.log('Accesso consentito!');
-    } else {
-        console.log('Accesso negato!');
+        if (grantAccess) {
+            console.log('Accesso consentito!');
+        } else {
+            console.log('Accesso negato!');
+        }
     }
 }
 checkAccessImproved();
+
+/*
+Questo codice serve per verifica se la mail inserita dall utente e compresa in quelle salvate nell 'array
+Errore:
+1. Mancava la chiusura della graffa alla funzione
+*/
 
 
 
